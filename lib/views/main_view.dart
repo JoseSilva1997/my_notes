d45b9b0614bb64690f7653cd328c837d741c6ae1
JoseSilva1997 <162315510+JoseSilva1997@users.noreply.github.com>
2025-07-13 15:45:10 +0100

    Main page boilerplate and navigation

--- lib/views/main_view.dart
+++ lib/views/main_view.dart
@@ -11,13 +11,55 @@ class MainView extends StatefulWidget{
 }
 
 class _MainViewState extends State<MainView> {
+  int _selectedIndex = 0;
+
+  // Placeholder widgets for each page
+  static const List<Widget> _pages = <Widget>[
+    Center(child: Text('Main Page')),
+    Center(child: Text('My Notes')),
+    Center(child: Text('To-Do List')),
+  ];
+
+  static const List<String> _titles = [
+    'Hub',
+    'My Notes',
+    'To-Do',
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    const pageTitle = PageTitle('Notes hub');
+    PageTitle pageTitle = PageTitle(_titles[_selectedIndex]);
     return Scaffold(
       appBar: AppBar(
         title: pageTitle,
         toolbarHeight: pageTitle.getHeight(),
+      ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          _onItemTapped(index);
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Hub',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.note),
+            label: 'My Notes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.check_box),
+            label: 'To-Do',
+          ),
+        ],
       ),
     );
   }
